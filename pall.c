61f58b832143a98d41cd3f6b2d118d2ed01526b8
savvyh <7277@holbertonstudents.com>
2024-01-03 10:04:35 +0000

    fix betty styleon the file pall.c to add description

--- pall.c
+++ pall.c
@@ -4,6 +4,7 @@
  * pall - Prints all the values on the stack.
  * @stack: Pointer to the top of the stack.
  * @line_number: The current line number in the bytecode file.
+ * Return: Nothing
  */
 void pall(stack_t **stack, unsigned int line_number)
 {

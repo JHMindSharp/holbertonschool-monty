f6a5b2156f9e57658a4ef3eb458ceb47a2f4c5dd
savvyh <7277@holbertonstudents.com>
2024-01-03 09:41:57 +0000

    fix betty style on the file monty.h

--- monty.h
+++ monty.h
@@ -16,9 +16,9 @@
  */
 typedef struct stack_s
 {
-    int n;
-    struct stack_s *prev;
-    struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -31,8 +31,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-    char *opcode;
-    void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 void push(stack_t **stack, unsigned int n);
@@ -43,7 +43,8 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 int is_numeric(char *str);
-void execute_instruction(char *line, stack_t **stack, unsigned int line_number);
+void execute_instruction(char *line, stack_t **stack,
+    unsigned int line_number);
 void free_stack(stack_t *stack);
 
 #endif /* MONTY_H */

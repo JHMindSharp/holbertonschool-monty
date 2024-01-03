817dfd2b8df4897407401db7e5e691246aadd4c3
savvyh <7277@holbertonstudents.com>
2024-01-03 12:52:17 +0000

    fix the function pall by check if current == NULL

--- pall.c
+++ pall.c
@@ -12,7 +12,10 @@ void pall(stack_t **stack, unsigned int line_number)
 
 	(void)line_number;
 
-	while (current != NULL)
+	if (current == NULL)
+		return;
+
+	while (current)
 	{
 		printf("%d\n", current->n);
 		current = current->next;

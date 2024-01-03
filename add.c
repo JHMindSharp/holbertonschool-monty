51e8b75a43a6024439549009886e8d8544b84e18
savvyh <7277@holbertonstudents.com>
2024-01-03 14:01:24 +0000

    fix the function add

--- add.c
+++ add.c
@@ -7,12 +7,22 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL || (*stack)->next == NULL)
+	stack_t *current = *stack;
+	int len = 0, temporary;
+
+	while (current)
+	{
+		current = current->next;
+		len++;
+	}
+	if (len < 2)
 	{
 		fprintf(stderr, "L%u: Error: can't add, stack too short\n", line_number);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
-
-	(*stack)->next->n += (*stack)->n;
+	current = *stack;
+	temporary = current->n + current->next->n;
+	current->next->n = temporary;
 	pop(stack, line_number);
 }

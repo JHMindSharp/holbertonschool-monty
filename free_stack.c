8aee2a7f51c54b1cbb508d8952053847baf3d109
savvyh <7277@holbertonstudents.com>
2024-01-03 09:53:07 +0000

    fix betty style on the file free_stack.c

--- free_stack.c
+++ free_stack.c
@@ -4,12 +4,14 @@
  * free_stack - Frees a stack.
  * @stack: The stack to be freed.
  */
-void free_stack(stack_t *stack) {
-    stack_t *temp;
+void free_stack(stack_t *stack)
+{
+	stack_t *temp;
 
-    while (stack != NULL) {
-        temp = stack;
-        stack = stack->next;
-        free(temp);
-    }
+	while (stack != NULL)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
 }

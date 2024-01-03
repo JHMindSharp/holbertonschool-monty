14ccb6a833bd390ed36665d5bdd75a988065c523
savvyh <7277@holbertonstudents.com>
2024-01-03 10:56:59 +0000

    fix the file nop.c by modify order

--- nop.c
+++ nop.c
@@ -4,9 +4,10 @@
  * nop - Does nothing.
  * @stack: Pointer to the top of the stack.
  * @line_number: The current line number in the bytecode file.
+ * Return: nothing
  */
-void nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **head, unsigned int line_number)
 {
-	(void)line_number;
-	(void)stack;
+	(void) line_number;
+	(void) head;
 }

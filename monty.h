0690699124ede04bdb5c10a891a87f4efe4eb39f
savvyh <7277@holbertonstudents.com>
2024-01-04 15:48:26 +0000

    fix a prototype function is_numeric

--- monty.h
+++ monty.h
@@ -46,7 +46,7 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
-int is_numeric(char *str);
+int is_numeric(const char *str);
 void execute_instruction(char *line, stack_t **stack,
 unsigned int line_number);
 void free_stack(stack_t *stack);

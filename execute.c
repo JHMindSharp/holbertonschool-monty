73007769567e5cc86c810538c42cd59f42a37116
savvyh <7277@holbertonstudents.com>
2024-01-04 11:15:32 +0000

    fix the file execute.c by adding 2 function one to handle error and one to execute the function push

--- execute.c
+++ execute.c
@@ -1,4 +1,33 @@
 #include "monty.h"
+/**
+ * handle_error - Handles error messages and exits the program
+ * @line_number: The current line number in the bytecode file
+ * @message: The error message to display
+ */
+void handle_error(unsigned int line_number, const char *message)
+{
+	fprintf(stderr, "L%u: Error: %s\n", line_number, message);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * execute_push - Executes the push instruction
+ * @stack: Pointer to the top of the stack
+ * @operand: The operand associated with the push instruction
+ * @line_number: The current line number in the bytecode file
+ */
+void execute_push(stack_t **stack, char *operand, unsigned int line_number)
+{
+	int value;
+
+	if (!operand || !is_numeric(operand))
+	{
+		handle_error(line_number, "usage: push integer");
+	}
+	value = atoi(operand);
+
+	push(stack, value);
+}
 
 /**
  * execute_instruction - Executes a Monty bytecode instruction
@@ -8,36 +37,21 @@
  * Return: nothing
  */
 void execute_instruction(char *instruction, stack_t **stack,
-unsigned int line_number)
+	unsigned int line_number)
 {
 	char *opcode, *operand;
 
 	if (!instruction || !stack)
-	{
-		fprintf(stderr, "L%u: Error: Invalid instruction or stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+		handle_error(line_number, "Invalid instruction or stack");
 
-	opcode = strtok(instruction, " \n");
+	opcode = strtok(instruction, " \t\n");
 	operand = strtok(NULL, " \t\n");
 
 	if (!opcode)
-	{
-		fprintf(stderr, "L%u: Error: Missing opcode\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+		handle_error(line_number, "Missing opcode");
 
 	if (strcmp(opcode, "push") == 0)
-	{
-		if (!operand || !is_numeric(operand))
-		{
-			fprintf(stderr, "L%u: Error: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
-		int value = atoi(operand);
-
-		push(stack, value);
-	}
+		execute_push(stack, operand, line_number);
 	else if (strcmp(opcode, "pall") == 0)
 		pall(stack, line_number);
 	else if (strcmp(opcode, "nop") == 0)
@@ -51,10 +65,7 @@ unsigned int line_number)
 	else if (strcmp(opcode, "pint") == 0)
 		pint(stack, line_number);
 	else
-	{
-		fprintf(stderr, "L%u: Error: Unknown instruction %s\n", line_number, opcode);
-		exit(EXIT_FAILURE);
-	}
+		handle_error(line_number, "Unknown instruction");
 }
 
 /**

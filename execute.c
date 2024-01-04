dff97c51e632dd097bf63a0c01def389868a0e58
savvyh <7277@holbertonstudents.com>
2024-01-04 09:27:00 +0000

    come back to my previous code

--- execute.c
+++ execute.c
@@ -41,11 +41,7 @@ unsigned int line_number)
 	else if (strcmp(opcode, "pall") == 0)
 		pall(stack, line_number);
 	else if (strcmp(opcode, "nop") == 0)
-	{
-		if (strcmp(opcode, "nope") == 0)
-			nop(stack, line_number);
 		nop(stack, line_number);
-	}
 	else if (strcmp(opcode, "add") == 0)
 		add(stack, line_number);
 	else if (strcmp(opcode, "swap") == 0)

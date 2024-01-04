3c039dd79de3cd1212fe06812287fb7ece8ec20d
savvyh <7277@holbertonstudents.com>
2024-01-04 13:25:22 +0000

    fix the file execute.c change operand by opcode

--- execute.c
+++ execute.c
@@ -18,7 +18,7 @@ void execute_instruction(char *instruction, stack_t **stack,
 		return;
 
 	if (strcmp(opcode, "push") == 0)
-		push(stack, line_number, operand);
+		push(stack, line_number, opcode);
 	else if (strcmp(opcode, "pall") == 0)
 		pall(stack, line_number);
 	else if (strcmp(opcode, "nop") == 0)

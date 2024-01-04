68135b07319c7572a2a550259cc97c9589396eb2
savvyh <7277@holbertonstudents.com>
2024-01-04 09:19:14 +0000

    come back to my previous code

--- add.c
+++ add.c
@@ -7,15 +7,12 @@
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	int result;
-
 	if (*stack == NULL || !*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%u: Error: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	result = ((*stack)->next->n + (*stack)->n);
+	(*stack)->next->n += (*stack)->n;
 	pop(stack, line_number);
-	(*stack)->n = result;
 }

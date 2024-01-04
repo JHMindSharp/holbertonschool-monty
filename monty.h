73a527d0bff8c109b9a8cc68b248022c4193d080
savvyh <7277@holbertonstudents.com>
2024-01-04 08:48:34 +0000

    fix the file monty.h by adding structure and prototype function

--- monty.h
+++ monty.h
@@ -37,18 +37,39 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+/**
+ * struct global_info_s - structure to store global information for
+ * Monty interpreter
+ * @monty_file: pointer to the currently open Monty file
+ * @current_line: pointer to the currently processed line in the Monty file
+ * @stack_head: pointer to the head of the stack
+ * @line_number: current line number being processed
+ *
+ * Description: This structure is used to store global information needed for
+ * the Monty interpreter, including the Monty file, current line, stack, and
+ * the line number being processed.
+ */
+typedef struct global_info_s
+{
+	FILE *monty_file;
+	char *current_line;
+	stack_t *stack_head;
+	unsigned int line_number;
+} global_info_t;
 
+extern global_info_t global_info;
 
-void push(stack_t **stack, unsigned int n);
+int execute_monty_instruction(char *instruction, stack_t **stack_head,
+unsigned int line_number);
+void push(stack_t **stack, int n);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
-int is_numeric(char *str);
-void execute_instruction(char *line, stack_t **stack,
-    unsigned int line_number);
 void free_stack(stack_t *stack);
+int is_numeric(const char *str);
+void cleanup(global_info_t *info);
 
 #endif /* MONTY_H */
